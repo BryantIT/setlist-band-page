7269dffd6985a53db745a95d9b1fa322a5a70932
Bryant Richards <dragonseyeimages@gmail.com>
2023-12-02 11:04:56 -0600

    boilerplates project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer({
+  images: {
+    deviceSizes: [428, 540, 640, 768, 1024, 1120],
+  },
+  eslint: {
+    dirs: ['pages', 'components', 'lib', 'layouts', 'utils'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+})
